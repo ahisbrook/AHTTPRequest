f3313cd2bb566122ba8a3a05a26715aeda218039
Adrienne Hisbrook <ahisbrook@gmail.com>
2013-07-09 22:03:15 -0500

    Updated

--- AHTTPRequest.m
+++ AHTTPRequest.m
@@ -1,24 +1,22 @@
 //
 //  AHTTPRequest.m
-//
 //  Created by Adrienne Hisbrook on 6/13/13.
 //
 
 #import "AHTTPRequest.h"
 
-@interface AHTTPRequest ()
-
-@property (nonatomic) NSURLRequest *theRequest;
-
+@protocol AHTTPRequestAsyncDelegate <NSObject>
+- (void)connectionEnded:(AHTTPRequest*)request;
 @end
 
 @implementation AHTTPRequest
+static NSURLRequest* _theRequest = nil;
 
 -(void)executeRequest {
-    self.theRequest = (NSURLRequest*)self;
+    _theRequest = (NSURLRequest*)self;
     NSHTTPURLResponse *tempResponse;
     NSError *tempErr;
-    NSData *tempData = [NSURLConnection sendSynchronousRequest:self.theRequest returningResponse:&tempResponse error:&tempErr];
+    NSData *tempData = [NSURLConnection sendSynchronousRequest:_theRequest returningResponse:&tempResponse error:&tempErr];
     
     if (tempData) {
         [self connectionDidEndWithResponse:tempResponse data:tempData error:tempErr];
@@ -26,11 +24,14 @@
 }
 
 -(void)executeRequestAsync:(id<AHTTPRequestAsyncDelegate>)delegate {
-    self.theRequest = (NSURLRequest*)self;
-    [NSURLConnection sendAsynchronousRequest:self.theRequest
+    _theRequest = (NSURLRequest*)self;;
+    [NSURLConnection sendAsynchronousRequest:_theRequest
                                        queue:[NSOperationQueue mainQueue]
-                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-                               [self connectionDidEndWithResponse:response data:data error:connectionError];
+                           completionHandler:^(NSURLResponse *response,
+                                               NSData *data,
+                                               NSError *error) {
+                               [self connectionDidEndWithResponse:response data:data error:error];
+                               [delegate connectionEnded:self];
                            }];
 }
 
@@ -41,7 +42,7 @@
         self.responseStatusCode = [NSNumber numberWithInt:httpResponse.statusCode];
         NSLog(@"Response received");
     }
-    if (data != nil && data.length > 0) {
+    if (data) {
         self.responseData = [NSMutableData dataWithData:data];
         
         NSError *err;
@@ -66,6 +67,64 @@
     NSError *err;
     NSData *bodyData = [NSJSONSerialization dataWithJSONObject:bodyDictionary options:NSJSONWritingPrettyPrinted error:&err];
     self.HTTPBody = bodyData;
+}
+
+/*
+ Convenience method for creating API requests
+ NOTE: 'methodName' refers to the API method, and 'httpMethod' is the http method
+ 
+ Example:
+ 
+ [AHTTPRequest requestWithURLString:@"http://www.myservicehost.com/api/"
+ methodName:@"Users/GetUser"
+ parameters:@{@"userid":@"jdoe42"}
+ httpMethod:@"GET"
+ useQueryStrings:YES];
+ 
+ would return a get request with a url of "http://www.myservicehost.com/api/Users/GetUser?userid=jdoe42"
+ */
+
++(AHTTPRequest*)requestWithURLString:(NSString*)baseUrl
+                          methodName:(NSString*)methodName
+                          parameters:(NSDictionary*)params
+                          httpMethod:(NSString*)httpMethod
+                     useQueryStrings:(BOOL)qs
+{
+    AHTTPRequest *request = [AHTTPRequest new];
+    
+    NSString *urlString = [baseUrl stringByAppendingString:methodName];
+    
+    if (qs) {
+        NSString *queryString = @"?";
+        int i = 0;
+        for (NSString *key in [params keyEnumerator]) {
+            queryString = [queryString stringByAppendingFormat:@"%@=%@",key,[params objectForKey:key]];
+            i++;
+            if (i < [params count]) {
+                queryString = [queryString stringByAppendingString:@"&"];
+            }
+        }
+        urlString = [urlString stringByAppendingString:queryString];
+    }
+    else {
+        if (params != nil) {
+            [request setHTTPBodyWithDictionary:params];
+        }
+    }
+    NSLog(@"URL: %@", urlString);;
+    [request setURL:[NSURL URLWithString:urlString]];
+    
+    // Set request options
+    [request setHTTPMethod:httpMethod];
+    
+    NSMutableDictionary *headers = [NSMutableDictionary new];
+    [headers setObject:@"application/json" forKey:@"Accept"];
+    [headers setObject:@"application/json" forKey:@"content-type"];
+    [headers setObject:[NSString stringWithFormat:@"%d",request.HTTPBody.length] forKey:@"content-length"];
+    
+    [request setAllHTTPHeaderFields:headers];
+    
+    return request;
 }
 
 @end
